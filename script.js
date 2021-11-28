b757d7ce6813a9af7dff5ccaf254ed5e7fc38e13
reallyhategithub <castleracer@outlook.com>
2021-11-28 11:56:00 -0600

    First working version, need to add if cases for each operator

--- script.js
+++ script.js
@@ -15,29 +15,50 @@ const redAdd = (previousValue, nextValue) => previousValue + nextValue;
 const redSub = (previousValue, nextValue) => previousValue - nextValue;
 const redTimes = (previousValue, nextValue) => previousValue * nextValue;
 const redDivide = (previousValue, nextValue) => previousValue / nextValue;
+let a;
+let b;
+const array1 = [];
 
 digits.forEach(digit => {
 
     digit.addEventListener('click', function(){
         if(myScreen.textContent == 0) {
             previousValue = myScreen.textContent;
+            a = 0;
             myScreen.textContent = '';
         }
+        
+        if(myScreen.textContent == totalValue) myScreen.textContent = '';
+
         myScreen.textContent += digit.id;
+        
     })
     
 });
 
+let accumulator = 0;
+let newValue = 0;
+let totalValue = 0;
+
 operators.forEach(operator => {
     operator.addEventListener('click', function(){
-        
-        previousValue = myScreen.textContent;
-        myScreen.textContent
+        //YOU ONLY NEED THE TWO LAST NUMBERS IN THE ARRAY! YOU DON'T NEED TO DELETE THEM EACH INSTANCE!!! [0,1,2,3] get 3 + 2 = 5;
+        newValue = +myScreen.textContent;
 
-        if(operator.id == 'plus'){
+        accumulator = array1.reduce(function(a,b){
+            return a+b;
+        },0);
 
-            alert(operator.id);
-        }
+        array1.length = 0;
+        array1.push(+accumulator);
+        array1.push(+newValue)
+ //       b.join everything into 1 single Number, that way the  array that's housing 1,2,3, becomes 123. Afterwards, proceed with getting the numbeer  in the screen;
+    let total = array1.reduce(function(acc,curr){
+        return acc + curr;
+    },0);
+
+    myScreen.textContent = total;
+    totalValue = total;
     })
 });
 
